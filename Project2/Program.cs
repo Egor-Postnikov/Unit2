b0d6ea177699b0d3e9a0cf365135642a31fb4c81
Egor Postnikov <e.postnikov@carx-tech.com>
2025-04-25 15:27:09 +0300

    2.2

---
+++ Project2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 5;
+            int c = 10;
+            if (b > a)
+            {
+                Console.WriteLine($"Число {b} больше числа {a}");
+            }
+            else if (b < a)
+            {
+                Console.WriteLine($"Число {b} меньше числа {a}");
+            }
+            else
+            {
+                Console.WriteLine($"Число {b} равно числу {a}");
+            }
+
+
+            for (int i = 1; i < 10;i++)
+            {
+                Console.WriteLine(i);
+
+
+            }
+
+           
+            
+           
+         
+        }
+        
+    }
+}
