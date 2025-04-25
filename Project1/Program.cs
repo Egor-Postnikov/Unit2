370382a02445078a8347456806672cad8eef3dd7
Egor Postnikov <e.postnikov@carx-tech.com>
2025-04-25 13:40:47 +0300

    2.1

---
+++ Project1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b;
+            b = 4;
+
+            byte b;
+            sbyte sb;
+            short s;
+            ushort us;
+            int i;
+            uint ui;
+            long l;
+            ulong ul;
+
+            float f = 3.4f;
+            double d= 2.9;
+
+            char c = '5';
+
+            string str = "Hello WORLD2";
+            
+            bool bl;
+
+        }
+    }
+}
